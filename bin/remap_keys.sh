b621720b75bf664dfe9486026ac1d468dc5f6186
Stefan Nožinić <stefan@lugons.org>
2025-05-24 12:30:04 +0200

    Add remap_keys script and launchd plist for key mapping

---
+++ bin/remap_keys.sh
@@ -0,0 +1,12 @@
+#!bin/bash 
+
+#!/bin/bash
+
+/usr/bin/hidutil property --set '{
+  "UserKeyMapping": [
+    {
+      "HIDKeyboardModifierMappingSrc": 0x700000064,
+      "HIDKeyboardModifierMappingDst": 0x700000035
+    }
+  ]
+}'
--- init.sh
+++ init.sh
@@ -1,8 +1,33 @@
 #!/bin/sh
 
-cp -r bin /home/
-cp -r sbin /home/
-cp functions /home/functions 
+is_osx() {
+    if [ "$(uname)" = "Darwin" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if is_osx; then
+    echo "Installing launchd agents"
+    mkdir -p ~/Library/LaunchAgents
+    cp -r launchd/* ~/Library/LaunchAgents/
+
+fi
+
+
+SUDO=""
+
+if is_osx; then 
+    sudo mkdir /opt/fantastixus/
+    sudo cp -r bin /opt/fantastixus/
+    sudo cp -r sbin /opt/fantastixus/
+    sudo cp functions /opt/fantastixus/functions
+else 
+    cp -r bin /home/
+    cp -r sbin /home/
+    cp functions /home/functions 
+fi 
 
 echo "Initialization finished"
 echo
